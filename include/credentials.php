660b0187bc1878c3d92d99fd1bf19ef64ded6e33
Russell Thackston <rthackston@georgiasouthern.edu>
2018-06-24 13:29:29 -0400

    Delete credentials.php

--- include/credentials.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Declare the credentials to the database
-$servername = "localhost";
-$serverusername = "root";
-$serverpassword = "zxNHATK8SG7pED3w7kFH";
-$serverdb = "IT5236-2019";
-
-?>
